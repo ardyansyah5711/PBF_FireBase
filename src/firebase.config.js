5641b5c170e94d272ac9fbb572a7d4bcee5fde0b
Ardyansyah Vira B <54975146+ardyansyah5711@users.noreply.github.com>
2021-04-17 12:58:13 +0700

    update tugas dan praktikum

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+    apiKey: " AIzaSyA2ZyE-1awYeuMNlEuQ4wgRkIZxC5Cs2Ew ",
+    authDomain: "pbf10-669a9.firebaseapp.com",
+    projectId: "pbf10-669a9",
+    storageBucket: "pbf10-669a9.appspot.com",
+    messagingSenderId: "322260295883",
+    appId: "1:322260295883:web:e3d77e083f093edcea7b4e",
+    measurementId: "G-HDBVTZE5E7"
+  };
